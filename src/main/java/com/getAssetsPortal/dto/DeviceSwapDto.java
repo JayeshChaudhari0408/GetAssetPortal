f3c2575a94e1e555a4d095e539102fbc10ed8bd3
Jayesh Chaudhari <jchaudhari204@gmail.com>
2026-01-21 16:52:54 +0530

    Assets Swap functionality Added

---
+++ src/main/java/com/getAssetsPortal/dto/DeviceSwapDto.java
@@ -0,0 +1,21 @@
+package com.getAssetsPortal.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeviceSwapDto {
+
+    @NotNull
+    private String serialNo;
+
+    private Long toUserId;
+
+    private String remark;
+}
